0432badfe69809f4165660d6abcc20bd07bafca2
mohamad pishdar <mohamadpishdar@gmail.com>
2023-10-21 09:06:52 +0330

    Delete Practice directory

--- Practice/Codes/fn.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-<title>Frame Navigation Example</title>
-</head>
-<frameset COLS="*,*">
-<frame NAME="left" SRC="left.php">
-<frame NAME="right" SRC="about:blank">
-</frameset><noframes></noframes>
-</html>
